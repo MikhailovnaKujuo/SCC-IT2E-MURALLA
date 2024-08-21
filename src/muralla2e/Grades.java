60ba5a84e1d1b6f9e652744d3cdcf2ebaa7666be
JessaTangpos <tangposjessa@gmail.com>
2024-08-21 17:03:51 +0800

    grades

---
+++ src/muralla2e/Grades.java
@@ -0,0 +1,27 @@
+
+package muralla2e;
+
+public class Grades {
+    String name;
+    int id;
+    double p, m, pf, f;
+    
+    public void addGrades(String name, int id, double p, double m, double pf, double f){
+        this.id = id;
+        this.name = name;
+        this.p = p;
+        this.m = m;
+        this.pf = pf; 
+        this.f = f;
+    }
+    
+    public void viewGrades(){
+        double average = (this.p + this.m + this.pf + this.f) / 4;
+        String remarks = (average > 3.0) ? "Failed" : "Passed";
+        
+        System.out.printf("%-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s\n",
+                this.id, this.name, this.p, this.m, this.pf, this.f, average, remarks);
+    }
+}
+
+}
--- src/muralla2e/Muralla2E.java
+++ src/muralla2e/Muralla2E.java
@@ -5,9 +5,13 @@ import java.util.Scanner;
 public class Muralla2E {
 
     public static void main(String[] args) {
-        Sales sell = new Sales();
+        Sales sell = new Sales();    
+//        sell.get_sales();
+
+        Grades gr = new Grades();
         
-        sell.get_sales();
+        gr.addGrades("Mike", 1001, 3.0, 2.0, 1.2, 1.7);
+        gr.viewGrades();
     }
     
 }
