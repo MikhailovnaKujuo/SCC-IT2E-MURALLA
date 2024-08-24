d67b0e9d265d6652edc3afda6e8837bbdda58a8e
MikhailovnaKujuo <mikhailryanmuralla@gmail.com>
2024-08-24 16:24:50 +0800

    Objects Array

--- src/muralla2e/Muralla2E.java
+++ src/muralla2e/Muralla2E.java
@@ -1,26 +1,36 @@
 package muralla2e;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Muralla2E {
 
     public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
         Sales sell = new Sales();    
 //        sell.get_sales();
 
-        Grades gr = new Grades();
-        
-//        gr.addGrades("Mike", 1001, 3.0, 2.0, 1.2, 1.7);
-//        gr.viewGrades();
+//        Grades[] gr = new Grades[2];
+//        gr[0] = new Grades();
+//        gr[1] = new Grades();
 //        
-        Products pr = new Products();
-        Products pr1 = new Products();
+//        gr[0].addGrades("Mike", 1001, 1.0, 2.0, 1.5, 1.2);
+//        gr[1].addGrades("Dum ahh", 1002, 3.5, 3.0, 3.4, 2.8);
+//        
+//        for (Grades grade : gr) {
+//            grade.viewGrades();
+//        }
         
-        pr.addProducts(1001, "Kopiko", 13, 20, 3);
-        pr1.addProducts(1002, "Coke", 500, 0, 10);
-        pr.viewProducts();
-        pr1.viewProducts();
+        Products[] pr = new Products[2];
+        pr[0] = new Products();
+        pr[1] = new Products();
         
-    }
-    
+        pr[0].addProducts(1001, "Kopiko", 13, 20, 3);
+        pr[1].addProducts(1002, "Coke", 500, 0, 10);
+        
+        for (Products prod : pr) {
+            prod.viewProducts();
+        }  
+        
+    }   
 }
