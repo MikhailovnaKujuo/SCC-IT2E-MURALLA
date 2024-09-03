2b4cb8625412d4d2e34b92d577d45ab991006c73
MikhailovnaKujuo <mikhailryanmuralla@gmail.com>
2024-09-03 23:28:10 +0800

    changes..

--- src/muralla2e/Muralla2E.java
+++ src/muralla2e/Muralla2E.java
@@ -13,29 +13,26 @@ public class Muralla2E {
         
         int choice;
         
-        do {
-            System.out.println("1. Sales\n2. Grades\n3. Products");
-            System.out.print("Choice: ");
-            choice = scan.nextInt();
-            System.out.println("");
-            
-            switch (choice) {
-                case 0:
-                    break;
-                case 1: 
-                    sell.get_sales();
-                    break;
-                case 2:
-                    gr.getGrades();
-                    break;
-                case 3:
-                    pr.getProducts();
-                    break;
-                default:
-                    System.out.println("????????\n");
-                    break;
-            }
-        } while (choice > 3 || choice < 0);
-          
+        System.out.println("1. Sales\n2. Grades\n3. Products");
+        System.out.print("Choice: ");
+        choice = scan.nextInt();
+        System.out.println("");
+
+        switch (choice) {
+            case 0:
+                break;
+            case 1: 
+                sell.get_sales();
+                break;
+            case 2:
+                gr.getGrades();
+                break;
+            case 3:
+                pr.getProducts();
+                break;
+            default:
+                System.out.println("????????\n");
+                break;
+        }         
     }   
 }
